1955c684e91bcd93a68349778c22025f304fbeac
Jacqueline <j.hargrove09@gmail.com>
2022-05-30 18:06:19 +0000

    added question pleasentries

---
+++ linux.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Please enter your full name:"
+read name
+echo "Reading.... thank you for patiently waiting"
+sleep 3 
+echo "-----------------------------"
+echo "Your username is:" && whoami 
+echo "-----------------------------"
+echo "This is one of my first Linux Scripts"
+sleep 2
+echo "Thank you for waiting while we process the date......."
+sleep 3
+echo "Made on:" && date
+echo "-----------------------------"
+sleep 2
+echo "My IP address is:"
+echo "-----------------------------"
+sleep 2
+curl ifconfig.me
+
+This is week 4 - project test
